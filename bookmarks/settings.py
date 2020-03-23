86feb4ea62d0779f29261dd0c826979abb483acf
Siddharth Isaiah <siddharthisaiah@gmail.com>
2020-03-24 00:02:44 +0530

    Create Images app and model

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
