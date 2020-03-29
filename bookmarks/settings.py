539f98c6e29735abf335158c1cc861ac2ecaf6c5
Siddharth Isaiah <siddharthisaiah@gmail.com>
2020-03-29 14:08:01 +0530

    Create bookmarklet and save to db

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = [
     'mysite.com',
     'localhost',
     '127.0.0.1',
+    '558e794e.ngrok.io',
 ]
 
 
---
+++ images/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'images'
+
+urlpatterns = [
+    path('create/', views.image_create, name='create'),
+    
+    # ex: detail/4/image-slug/
+    path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
+]
